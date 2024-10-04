401c71b676552dbf5d846e3917ac08c715fa36bf
PeerJack22 <pajocina.11@gmail.com>
2024-10-04 11:59:14 -0500

    Nueva version 0.0.0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("---------------BIENVENIDO-------------");
         System.out.println("--------------------------------------");
         for (int i = 1000; i <= 2002; i=i+17 ) {
             System.out.println(i);
