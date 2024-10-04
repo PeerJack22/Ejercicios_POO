5356896db79fd253ade76f277e7b435ad51de018
PeerJack22 <pajocina.11@gmail.com>
2024-10-04 11:56:25 -0500

    Nueva version

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-------------------------------------");
+        System.out.println("--------------------------------------");
         for (int i = 1000; i <= 2002; i=i+17 ) {
             System.out.println(i);
         }
@@ -8,7 +8,7 @@ public class Main {
         for (int i=0; i <=50; i++) {
             System.out.println(i +" .-Paulo Cisneros");
         }
-        System.out.println("-------------------------------------");
+        System.out.println("--------------------------------------");
         for (int i=0; i <=75; i++) {
 
             System.out.println(i);
